e2bfc48ded52d708740aeb3069251bbce2a21fcb
Pratham Garg <140713281+150202-Pratham@users.noreply.github.com>
2025-02-14 08:53:02 +0530

    Solution to Print a Reverse Alphabet Triangle

--- Print a Reverse Alphabet Triangle@@67877e1818d53eb724f435cb/code.c
+++ Print a Reverse Alphabet Triangle@@67877e1818d53eb724f435cb/code.c
@@ -4,7 +4,7 @@ int main(){
     int n ; 
     scanf("%d" , &n) ;
 
-    for(int i = n; i>=0 ; i--){
+    for(int i = n; i>0 ; i--){
         for(int j = 65 ; j<65+i; j++ ){
             printf("%c ", j);
             
