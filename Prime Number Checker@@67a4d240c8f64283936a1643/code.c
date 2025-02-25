e1095f87c8aeb37ea990ba1c4235f3fe1d69a7a5
Pratham Garg <140713281+150202-Pratham@users.noreply.github.com>
2025-02-25 10:40:43 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,26 @@
+int isPrime(int n){
+
+    if(n<=1){
+        return 0 ;
+
+    }
+
+    else{
+
+        for(int i = 2 ; i<=n ; i++){
+            if(n%i==0){
+                return 1 ;
+
+
+            }
+
+
+        }
+
+
+    }
+
+    return 0 ;
+
+    
+}
