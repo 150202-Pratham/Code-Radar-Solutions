20403b8f20ec9438dd00f0ce051210be1e76aa66
Pratham Garg <140713281+150202-Pratham@users.noreply.github.com>
2025-04-01 11:45:59 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,8 +1,14 @@
 void fibonacciSeries(int n){
-
+   
     int a = 0 ;
     int b = 1 ;
     int i = 1 , c ;
+
+
+    if(n==1){
+
+        printf("0");
+    }
     printf("%d %d" , a , b);
 
     while(i<=n-2){
