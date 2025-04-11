3ebb784169a151b56b6c12f0f16c9dad583d3459
Pratham Garg <140713281+150202-Pratham@users.noreply.github.com>
2025-04-11 13:05:45 +0530

    Solution to Concatenate Two Strings

--- Concatenate Two Strings@@676c039d80e1714600f67644/code.c
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -7,7 +7,10 @@ int main(){
     char a[1000] ;
     char b[1000] ;
 
-
+     
+    fgets(a , 1000, stdin);
+    fgets(b , 1000 , stdin) ;
+    
     // By default you cannot concatenate the two strings togehter you need to iterate through the String 
 
     // Removing the null chardacter from first String 
@@ -31,10 +34,11 @@ int main(){
 
 
     }
-
+    
+    a[i] = '\0' ;
+    
     printf("%s" , a) ;
     return 0 ;
 
-    
 
 }
